0e5372a913eb88e4cbb12dc8c49acc9be28efd9f
Stark032702 <ak1867@scarletmail.rutgers.edu>
2023-04-04 17:48:53 -0400

    fixed some syntax errors

--- libraries/AP_Motors/AP_MotorsMatrix_6DoF_Scripting.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix_6DoF_Scripting.cpp
@@ -13,7 +13,7 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/#if AP_SCRIPTING_ENABLED
+#if AP_SCRIPTING_ENABLED
 
 #include <AP_HAL/AP_HAL.h>
 #include "AP_MotorsMatrix_6DoF_Scripting.h"
