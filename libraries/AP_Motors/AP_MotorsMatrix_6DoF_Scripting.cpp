6682fdb99bea8d012d57818ec0df054e544a6e2b
blank <ak1867@scarletmail.rutgers.edu>
2023-04-12 22:02:59 +0000

    things

--- libraries/AP_Motors/AP_MotorsMatrix_6DoF_Scripting.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix_6DoF_Scripting.cpp
@@ -18,7 +18,7 @@
 #include "AP_MotorsMatrix_6DoF_Scripting.h"
 #include <AP_Vehicle/AP_Vehicle.h>
 #include <SRV_Channel/SRV_Channel.h>
-
+#define   AP_MOTORS_MOT_5       CH_5
 extern const AP_HAL::HAL& hal;
 
 void AP_MotorsMatrix_6DoF_Scripting::output_to_motors()
@@ -323,7 +323,7 @@ bool AP_MotorsMatrix_6DoF_Scripting::init(uint8_t expected_num_motors){
 bool AP_MotorsMatrix_6DoF_Scripting:: init(uint8_t expected_num_motors){  
     add_motor(AP_MOTORS_MOT_1,     0,              0,              0.71f,            1.0f,             0,                0,               false,1); 
     add_motor(AP_MOTORS_MOT_2,     0,              0,              -0.71f,           1.0f,             0,                0,               false,2); 
-    add_motor(AP_MOTORS_MOT_5,     0,              0,              0.71f,            1.0f,             0,                0,                false,3); 
+    add_motor(CH_5,     0,              0,              0.71f,            1.0f,             0,                0,                false,3); 
     SRV_Channels::set_angle(SRV_Channels::get_motor_function(AP_MOTORS_MOT_5), 90*100);
     add_motor(AP_MOTORS_MOT_4,     0,              0,              -0.71f,           1.0f,             0,                0,                false,4); 
     set_update_rate(400); 
