6475b54205d01e4fc44274511a615f7aa9c6257f
Stark032702 <ak1867@scarletmail.rutgers.edu>
2023-07-05 19:35:52 -0400

    trying more things

--- coeff_matrix.py
+++ coeff_matrix.py
@@ -2,36 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpb 
 
-mu = (1.6*(10**-6))
-Km = (2.58 *(10**-8))
+mu = -(1.6*(10**-6))/(1.94*9.81*0.25*(100/70))
+Km = (2.58 *(10**-8))/(1.94*0.5*(100/70)*9.81*0.16)
 L = 0.16
 c = 0.707106781
+A = c * L * mu
 
 coefficient_matrix = [
-       [float(mu), 0.0,0.0, -float(mu), 0.0,0.0,float(mu), 0.0,0.0, -float(mu), 0.0, 0.0], 
-       [ 0.0, -float(mu),0.0, 0.0, float(mu), 0.0, 0.0, -float(mu), 0.0, 0.0, float(mu), 0.0], 
-       [ 0.0, 0.0, float(mu), 0.0,0.0, -float(mu), 0.0,0.0, float(mu), 0.0,0.0, -float(mu)], 
-       [-float(Km),0.0,float(c * mu * L ), -float(Km),0.0, -float(c* L * mu), -float(Km),0.0, -float(c*L*mu), -float(Km),0.0, float(c* L * mu)], 
-       [float(Km),0.0,-float(c * mu * L ),float(Km),0.0, -float(c* L * mu), float(Km),0.0, float(c*L*mu), float(Km),0.0, float(c * L * mu)], 
-       [-float(c * L * mu),-float(c * L * mu), -float(Km), float(c * L * mu),-float(c * L * mu), -float(Km), float(c * L * mu),float(c * L * mu), -float(Km ), -float(c * L * mu),float(c * L * mu), -float(Km)]
+       [-float(mu), 0.0,0.0, -float(mu), 0.0,0.0,-float(mu), 0.0,0.0, -float(mu), 0.0, 0.0], 
+       [ 0.0, -float(mu),0.0, 0.0, -float(mu), 0.0, 0.0, -float(mu), 0.0, 0.0, -float(mu), 0.0], 
+       [ 0.0, 0.0, float(mu), 0.0,0.0, float(mu), 0.0,0.0, float(mu), 0.0,0.0, float(mu)], 
+       [float(Km),0.0,float(A ), float(Km),0.0, -float(A), -float(Km),0.0, -float(A), -float(Km),0.0, float(A)], 
+       [0.0,-float(Km),-float(A ),0.0,-float(Km), float(A),0.0, float(Km), -float(A), 0.0,float(Km), float(A)], 
+       [-float(A),-float(A), float(Km), float(A),float(A), float(Km), float(A),-float(A), -float(Km ), -float(A),float(A), -float(Km)]
+    ]
+C = 0.0
+coefficient_matrix2 = [
+       [float(mu)+ C, 0.0,0.0, float(mu)+C, 0.0,0.0,float(mu)+C, 0.0,0.0, float(mu)+C, 0.0, 0.0], 
+       [ 0.0, float(mu)+C,0.0, 0.0, float(mu)+C, 0.0, 0.0, float(mu)+C, 0.0, 0.0, float(mu)+C, 0.0], 
+       [ 0.0, 0.0, float(mu)+C, 0.0,0.0, float(mu)+C, 0.0,0.0, float(mu)+C, 0.0,0.0, float(mu)+C], 
+       [-float(Km),0.0,float(A ), -float(Km),0.0, -float(A), -float(Km),0.0, -float(A), -float(Km),0.0, float(A)], 
+       [float(Km),0.0,-float(A ),float(Km),0.0, -float(A), float(Km),0.0, float(A), float(Km),0.0, float(A)], 
+       [-float(A),-float(A), -float(Km), float(A),-float(A), -float(Km), float(A),float(A), -float(Km ), -float(A),float(A), -float(Km)]
     ]
 
 coeff_array = np.array(coefficient_matrix)
-points = 150
-noise = np.random.normal(0.01,0.002,6*points).reshape((6,points))
-noise[2][:] = 0.5
-noise[3][:] = 0
-noise[4][:] = 0
-noise
+points = 500
+noise = np.random.normal(0.01,0.02,6*points).reshape((6,points))
+noise[1] = np.linspace(0.01, 1.0 ,points)
+noise[3] = np.linspace(0.01, 1.0, points)
 outputs = np.linalg.pinv(coeff_array).dot(noise)
 np.set_printoptions(suppress=True)
-#print(np.linalg.pinv(coeff_array).T)
-
+#print(repr(np.linalg.pinv(coeff_array).T.reshape((72,))))
+print(np.linalg.cond(np.linalg.pinv(coeff_array)))
 angular_velocity = np.sqrt(np.abs(outputs[2:outputs.shape[0]:3]))/1500
 
 alpha_angles = np.zeros(outputs.shape)
 beta_angles = np.zeros(outputs.shape)
-lowpass_filter = 1
+lowpass_filter = 0
 previous_beta = [0,0,0,0] 
 previous_alpha = [0,0,0,0] 
 for j in range(0, outputs.shape[1]):
@@ -48,9 +56,9 @@ for j in range(0, outputs.shape[1]):
             counter+=1
         else: 
             alpha_angles2 = (outputs[i,j]/outputs[i+2,j]) 
-            alpha_angles[i//3,j] = 0.1 * (np.remainder(alpha_angles2, np.pi) * (180/np.pi) - previous_alpha[counter]) +(previous_alpha[counter])
+            alpha_angles[i//3,j] = 0.5 * (np.remainder(alpha_angles2, np.pi) * (180/np.pi) - previous_alpha[counter]) +(previous_alpha[counter])
             beta_angles2 = (outputs[i+1,j]/outputs[i+2,j]) 
-            beta_angles[i//3,j] = 0.1 *(np.remainder(beta_angles2, np.pi) * (180/np.pi) - previous_beta[counter]) + (previous_beta[counter])
+            beta_angles[i//3,j] = 0.5 *(np.remainder(beta_angles2, np.pi) * (180/np.pi) - previous_beta[counter]) + (previous_beta[counter])
             previous_alpha[counter] = alpha_angles[i//3,j]
             previous_beta[counter] = beta_angles[i//3,j] 
             counter+=1   
@@ -59,8 +67,8 @@ fig,axes = plt.subplots(ncols=4, nrows=2, figsize=(5,6))
 counter = 1
 for row in range(2): 
     for col in range(2): 
-        axes[row, col].plot(time, alpha_angles[counter-1][:], color='red', label="pitch angle")
-        axes[row, col].plot(time, beta_angles[counter-1][:], color='blue', label="roll angle")
+        axes[row, col].plot(noise[3], alpha_angles[counter-1][:], color='red', label="pitch angle")
+        axes[row, col].plot(noise[3], beta_angles[counter-1][:], color='blue', label="roll angle")
         axes[row, col].legend()
         axes[row, col].set_title(f"Servo Angles for Motor {counter}")
         axes[row, col].set_xlabel("points(t)")
